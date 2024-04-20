df206f96d32db3df63c86fe73d3ecd8cf4b54d8f
Ryan Skinner <ryan@42north.ca>
2024-04-20 12:52:35 -0400

    linting

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,5 @@
+import process from 'node:process'
+
 export const API_KEY = process.env.MTA_API_KEY
 export const ALERT_FEED_URL = process.env.MTA_API_URL
 export const BSKY_USERNAME = process.env.BSKY_USERNAME
--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,3 @@
 const antfu = require('@antfu/eslint-config').default
 
-module.exports = antfu({
-  rules: {
-    'node/prefer-global/process': 'off',
-  },
-})
+module.exports = antfu()
